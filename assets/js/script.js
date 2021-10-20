fde7a665fd7ef8595650a0781f888710758d0a82
brt9029 <brt9029@gmail.com>
2021-10-20 12:10:40 -0500

    added proper div li creation for tasks to appear with the type and value provided

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,22 @@ let tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
     event.preventDefault();
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // create list item
     let listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    let taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 }
 
